05ff6f2ded16cc5cea5ce01b5da262d194caae53
shovo <mdhassy753@gmail.com>
2019-05-17 17:19:17 +0600

    Generated different sizes layout for login and registration layout

--- app/src/main/java/com/example/csestudentmate/MainActivity.java
+++ app/src/main/java/com/example/csestudentmate/MainActivity.java
@@ -8,8 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.theme));
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setLogo(getDrawable(R.drawable.logo_white));
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/csestudentmate/RegistrationPage.java
+++ app/src/main/java/com/example/csestudentmate/RegistrationPage.java
@@ -1,11 +1,22 @@
 package com.example.csestudentmate;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class RegistrationPage extends AppCompatActivity {
+public class RegistrationPage extends AppCompatActivity implements View.OnFocusChangeListener {
+
+    private Button signUpButton;
+
+    private TextInputEditText fullnameText, usernameText, emailText;
+    private TextInputEditText contactNoText, passwordText, confirmPasswordText;
+
+    private String fullname, username, email;
+    private String contactNo, password, confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +25,20 @@ public class RegistrationPage extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_registration_page);
+
+        signUpButton = findViewById(R.id.signUpId);
+
+        fullnameText = findViewById(R.id.fullnameId);
+        usernameText = findViewById(R.id.registerUsernameId);
+        emailText = findViewById(R.id.emailId);
+        contactNoText = findViewById(R.id.contactNoId);
+        passwordText = findViewById(R.id.registerPasswordId);
+        confirmPasswordText = findViewById(R.id.confirmPasswordId);
+
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        //
     }
 }
