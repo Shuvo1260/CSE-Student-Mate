ac7658052ec4058e34b247ff2071985c2ebe5e79
shovo <mdhassy753@gmail.com>
2019-05-20 00:32:44 +0600

    Navigation Drawaer Listener and Fragment transition all over things completed. Home, Class and NU_Result activity has created.

--- app/src/main/java/com/example/csestudentmate/MainActivity.java
+++ app/src/main/java/com/example/csestudentmate/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.csestudentmate;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private FragmentTransaction fragmentTransaction;
+    private Fragment fragment;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         acctionBarSeeting();
         navigationDrawerSettings();
+    }
 
+    private void fragmentSetup(int item_id) {
+
+        // This transaction replace one fragment to your desire fragment.
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.contentFrameId, fragment);
+        fragmentTransaction.commit();
+        navigationView.setCheckedItem(item_id); // Making item selectable.
+
+        // This condition is for close the navigation drawer if it is open.
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 
     private void navigationDrawerSettings() {
+
+        // This Layout use to access navigation drawer.
         drawerLayout = findViewById(R.id.drawerLayoutId);
 
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
@@ -33,14 +54,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
 
-        //Setting Navigation Drawer icon.
+        // Setting Navigation Drawer icon.
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_menu));
+
+        navigationView = findViewById(R.id.navigationDrawerId);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragment = new Home();
+        fragmentSetup(R.id.navHomeId);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int item_id = menuItem.getItemId();
+
+        fragment = null;
+
+        if(item_id == R.id.navHomeId){
+            fragment = new Home();
+        }else if(item_id == R.id.navClassId){
+            fragment = new Class();
+        }else if(item_id == R.id.navNUResultId){
+            fragment = new NU_Result();
+        }else{
+            fragment = new Home();
+        }
+
+        if(fragment != null){
+            fragmentSetup(item_id);
+        }
         return false;
     }
 
@@ -50,5 +94,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toolbar.setTitle("Alhamdulillah");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
