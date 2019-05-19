e624a408363917c332fe0df12c3009b514f84f38
shovo <mdhassy753@gmail.com>
2019-05-19 16:22:59 +0600

    Navigation Drawer Layout have added. And added some design change.

--- app/src/main/java/com/example/csestudentmate/MainActivity.java
+++ app/src/main/java/com/example/csestudentmate/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.csestudentmate;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.theme));
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
-//        getSupportActionBar().setLogo(getDrawable(R.drawable.logo_white));
-//        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
+        acctionBarSeeting();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    public void acctionBarSeeting(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbarId);
+        toolbar.setTitle("Alhamdulillah");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
+        setSupportActionBar(toolbar);
     }
 }
