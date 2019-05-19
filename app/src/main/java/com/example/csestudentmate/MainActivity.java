d7d44e34bd59d97fe97ed665e98a4e1d2428367c
shovo <mdhassy753@gmail.com>
2019-05-19 23:46:58 +0600

    Navigation Drawer icon has added and Navigation Drawer layout width set as match_parent.

--- app/src/main/java/com/example/csestudentmate/MainActivity.java
+++ app/src/main/java/com/example/csestudentmate/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.csestudentmate;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        acctionBarSeeting();
 
+        acctionBarSeeting();
+        navigationDrawerSettings();
+
+    }
+
+    private void navigationDrawerSettings() {
+        drawerLayout = findViewById(R.id.drawerLayoutId);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        //Setting Navigation Drawer icon.
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_menu));
     }
 
     @Override
@@ -23,7 +45,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     public void acctionBarSeeting(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbarId);
+        //Custom toolbar creating and setting.
+        toolbar = (Toolbar) findViewById(R.id.mainToolbarId);
         toolbar.setTitle("Alhamdulillah");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
         setSupportActionBar(toolbar);
